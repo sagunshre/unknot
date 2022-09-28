926c3522c00dbe362df752a7b5f3dafd8cb5457c
Sagun <sagunshre@gmail.com>
2022-09-29 00:39:04 +0200

    added configuration file

---
+++ data/target/metadata.py
@@ -0,0 +1,13 @@
+import csv
+import json
+from IPython import embed
+
+with open('metadata.csv', 'w') as meta_file, open('annotations/test.csv', 'r') as csv_file:
+  data = csv.reader(csv_file, delimiter=",")
+  fieldnames = ["filename","distance"]
+  next(data, None)
+  writer = csv.writer(meta_file)
+  writer.writerow(fieldnames)
+  for row in data:
+    attr = json.loads(row[14])
+    writer.writerow([row[8], float(attr['metadata']['area'])])
